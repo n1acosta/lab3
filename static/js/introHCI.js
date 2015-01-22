0dadaf8e4c14c3271b00f2ce8492b18822c15532
Nancy Acosta <acostanancy18@gmail.com>
2015-01-22 12:11:00 -0800

    step 54 of lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,28 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("I love COGS120!");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+function projectClick(e) {
+	// prevent the page from reloading
+	e.preventDefault();
+	// In an event handler, $(this) refers to
+	// the object that triggered the event
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else {
+    	$(description).fadeOut();
+    }
 }
